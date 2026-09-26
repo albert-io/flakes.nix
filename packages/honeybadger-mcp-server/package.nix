99fd58a9f2cf14601f6239b6350362de53bb3e27
alechartung <20519187+alechartung@users.noreply.github.com>
2026-09-26 03:13:59 +0000

    honeybadger-mcp-server: update

--- packages/honeybadger-mcp-server/package.nix
+++ packages/honeybadger-mcp-server/package.nix
@@ -6,19 +6,19 @@
 
 buildGoModule rec {
   pname = "honeybadger-mcp-server";
-  rev = "7620ad7403b5b4a6a849fcb98038f1ed84c24f0b";
+  rev = "5291ed67ae080550d8b4969c6d4519788f26d5c9";
   version = "unstable-${builtins.substring 0 7 rev}";
 
   src = fetchFromGitHub {
     owner = "honeybadger-io";
     repo = "honeybadger-mcp-server";
     inherit rev;
-    hash = "sha256-9/hZVqkXtMa1H8XXkQhy21HI5n9iVS08GUu5rAKYAwo=";
+    hash = "sha256-zyEcQNuJUiH0nrh5CuIsZfld/gOqwke6dslW0seaSrw=";
   };
 
   subPackages = [ "cmd/honeybadger-mcp-server" ];
 
-  vendorHash = "sha256-N4SI14Hm0bdLMilyZwqR70KToD2Xiav/u1uNsZJAOuw=";
+  vendorHash = "sha256-y0zY2hJAbVKMYVJXjF/xGBahVX0C+MCp5w2pi2LwL8o=";
 
   ldflags = [
     "-s"
